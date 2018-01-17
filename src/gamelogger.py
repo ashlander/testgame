0b303d1d05a8a003f2f55fdfd33fb78525eb22a0
Andrii Zhuk <_buddy@ukr.net>
2018-01-17 13:15:59 +0200

    Output to stdout instead of stderr

--- src/gamelogger.py
+++ src/gamelogger.py
@@ -1,14 +1,15 @@
+import sys
 import logging
 
 '''Initialize logger'''
 def init():
     logFormat = '%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s'
     logging.basicConfig(filename='_testgame.log', level=logging.DEBUG, format=logFormat)
-    
+
     logFormatter = logging.Formatter(logFormat)
     rootLogger = logging.getLogger()
 
-    consoleHandler = logging.StreamHandler()
+    consoleHandler = logging.StreamHandler(sys.stdout)
     consoleHandler.setFormatter(logFormatter)
     rootLogger.addHandler(consoleHandler)
 
