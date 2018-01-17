e4c01d529d550552a7f4bb10aaf6ae8946f28066
Andrii Zhuk <_buddy@ukr.net>
2018-01-17 13:13:30 +0200

    Logger output to stdout

--- src/gamelogger.py
+++ src/gamelogger.py
@@ -2,7 +2,16 @@ import logging
 
 '''Initialize logger'''
 def init():
-    logging.basicConfig(filename='_testgame.log', level=logging.DEBUG, format='%(asctime)s %(message)s')
+    logFormat = '%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s'
+    logging.basicConfig(filename='_testgame.log', level=logging.DEBUG, format=logFormat)
+    
+    logFormatter = logging.Formatter(logFormat)
+    rootLogger = logging.getLogger()
+
+    consoleHandler = logging.StreamHandler()
+    consoleHandler.setFormatter(logFormatter)
+    rootLogger.addHandler(consoleHandler)
+
     logging.info('#############################################')
     logging.info('#          Logger is initialized            #')
     logging.info('#############################################')
